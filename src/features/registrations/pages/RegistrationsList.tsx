f096a2c58262d667356e8f2d04301066576a1530
IYFOrlando <orlando@iyfusa.org>
2025-08-11 21:30:13 -0400

    Fix missing closing brace in RegistrationsList.tsx columns definition

--- src/features/registrations/pages/RegistrationsList.tsx
+++ src/features/registrations/pages/RegistrationsList.tsx
@@ -233,6 +233,7 @@ export default function RegistrationsList({ isAdmin = false, hasGmailAccess = fa
             </Stack>
           )
         }
+      }
     ],
     [byStudent, effectiveIsAdmin]
   )
